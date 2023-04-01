84f8f1fac0060339f654a29468c73b15f824d002
Diego Nahuel Villan <124027106+Nahuel409@users.noreply.github.com>
2023-03-31 22:21:30 -0300

    Update HnswIndex.java

--- ann/src/main/java/com/twitter/ann/hnsw/HnswIndex.java
+++ ann/src/main/java/com/twitter/ann/hnsw/HnswIndex.java
@@ -37,7 +37,7 @@ import com.twitter.search.common.file.AbstractFile;
  * Typed multithreaded HNSW implementation supporting creation/querying of approximate nearest neighbour
  * Paper: https://arxiv.org/pdf/1603.09320.pdf
  * Multithreading impl based on NMSLIB version : https://github.com/nmslib/hnsw/blob/master/hnswlib/hnswalg.h
- *
+ * @autor elon
  * @param <T> The type of items inserted / searched in the HNSW index.
  * @param <Q> The type of KNN query.
  */
